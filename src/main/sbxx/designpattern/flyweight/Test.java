3dfbb2c2bee47adb39751072d521c27853ac3f0c
dair <2997266712@qq.com>
2021-03-29 14:24:45 +0800

    设计模式-享元模式

---
+++ src/main/sbxx/designpattern/flyweight/Test.java
@@ -0,0 +1,36 @@
+package main.sbxx.designpattern.flyweight;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class Test {
+	
+	private static final  String[] colors={"Red", "Green", "Blue", "White", "Black" };
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 20; i++) {
+			Circle circle= (Circle) ShapeFactory.getMap(getRandomColor());
+			circle.setX(getRandomX());
+			circle.setY(getRandomY());
+			circle.setRadius(100);
+			circle.draw();
+		}
+	
+	}
+	
+	private static String getRandomColor(){
+		return colors[(int) (Math.random()*colors.length)];
+	}
+	
+	private static  int getRandomX(){
+		return (int) (Math.random()*100);
+	}
+	
+	private static int getRandomY(){
+		return (int) (Math.random()*100);
+	}
+	
+	
+}
