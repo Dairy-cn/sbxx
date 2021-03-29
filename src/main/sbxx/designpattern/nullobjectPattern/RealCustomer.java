d33c9210e95ce226dd64f34e72c402de2324c082
dair <2997266712@qq.com>
2021-03-29 15:23:08 +0800

    设计模式——空对象模式

---
+++ src/main/sbxx/designpattern/nullobjectPattern/RealCustomer.java
@@ -0,0 +1,22 @@
+package main.sbxx.designpattern.nullobjectPattern;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class RealCustomer extends AbstractCustomer {
+	
+	public RealCustomer(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public boolean isNull() {
+		return false;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+}
