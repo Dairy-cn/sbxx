9966da23a982a6d157097fd6b118181f36cf348c
dair <2997266712@qq.com>
2021-03-31 11:33:26 +0800

    工厂模式

---
+++ src/main/sbxx/designpattern/factory/ShapeFactory.java
@@ -0,0 +1,26 @@
+package main.sbxx.designpattern.factory;
+
+import java.awt.print.Pageable;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class ShapeFactory {
+	
+	
+	public Shape getShape(String shapeType) {
+		if (shapeType == null) {
+			return null;
+		}
+		if (shapeType.equalsIgnoreCase("CIRCLE")) {
+			return new Circle();
+		} else if (shapeType.equalsIgnoreCase("Rectangle")) {
+			return new Rectangle();
+		} else if (shapeType.equalsIgnoreCase("Square")) {
+			return new Square();
+		} else {
+			return null;
+		}
+	}
+}
---
+++ src/main/sbxx/designpattern/factory/Test.java
@@ -0,0 +1,18 @@
+package main.sbxx.designpattern.factory;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		ShapeFactory shapeFactory=new ShapeFactory();
+		Shape circle = shapeFactory.getShape("Circle");
+		circle.draw();
+		Shape square = shapeFactory.getShape("square");
+		square.draw();
+		Shape rectangle = shapeFactory.getShape("Rectangle");
+		rectangle.draw();
+	}
+}
