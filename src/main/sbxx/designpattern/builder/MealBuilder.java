b2c022ca4257613aee40b9684833db27ede40319
dair <2997266712@qq.com>
2021-04-01 14:17:25 +0800

    建造模式

---
+++ src/main/sbxx/designpattern/builder/MealBuilder.java
@@ -0,0 +1,23 @@
+package main.sbxx.designpattern.builder;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class MealBuilder {
+
+	public Meal getVegMeal(){
+		Meal meal=new Meal();
+		meal.addItem(new ChickenBurger());
+		meal.addItem(new Coke());
+		return meal;
+	}
+	
+	public Meal getNoVegMeal(){
+		Meal meal=new Meal();
+		meal.addItem(new VegBurger());
+		meal.addItem(new Pepsi());
+		return meal;
+	}
+
+}
--- src/main/sbxx/designpattern/builder/Test.java
+++ src/main/sbxx/designpattern/builder/Test.java
@@ -7,17 +7,13 @@ package main.sbxx.designpattern.builder;
 public class Test {
 	
 	public static void main(String[] args) {
-		Meal meal=new Meal();
-		meal.addItem(new Pepsi());
-		meal.addItem(new VegBurger());
-		meal.showItems();
-		System.out.println("套餐一金额总额："+meal.getPrice());
-		meal=new Meal();
-		meal.addItem(new Coke());
-		meal.addItem(new ChickenBurger());
-		meal.showItems();
+		Meal vegMeal=new MealBuilder().getVegMeal();
+		vegMeal.showItems();
+		System.out.println("套餐一金额总额："+vegMeal.getPrice());
 		
-		System.out.println("套餐二金额总额："+meal.getPrice());
+		Meal noVegMeal=new MealBuilder().getNoVegMeal();
+		noVegMeal.showItems();
+		System.out.println("套餐二金额总额："+noVegMeal.getPrice());
 		
 	}
 }
