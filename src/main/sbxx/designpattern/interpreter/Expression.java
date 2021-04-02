0cc1375bae6801d263b1c28436dbb1ba8a84906a
dair <2997266712@qq.com>
2021-04-02 16:00:27 +0800

    解释器模式

---
+++ src/main/sbxx/designpattern/interpreter/Expression.java
@@ -0,0 +1,11 @@
+package main.sbxx.designpattern.interpreter;
+
+/**
+ * @author Dair
+ * @since
+ */
+public interface Expression {
+	
+	public boolean interpret(String context);
+	
+}
---
+++ src/main/sbxx/designpattern/interpreter/TerminalExpression.java
@@ -0,0 +1,24 @@
+package main.sbxx.designpattern.interpreter;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class TerminalExpression implements Expression {
+	
+	
+	private String data;
+	
+	
+	public TerminalExpression(String data) {
+		this.data = data;
+	}
+	
+	@Override
+	public boolean interpret(String context) {
+		if (context.contains(data)) {
+			return true;
+		}
+		return false;
+	}
+}
