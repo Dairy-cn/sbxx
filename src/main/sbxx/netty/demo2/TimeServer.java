df6ecfb44fe07708a570e33025a06beb9e8b043c
dair <2997266712@qq.com>
2021-05-21 10:02:54 +0800

    netty demo

---
+++ src/main/sbxx/netty/demo2/TimeServer.java
@@ -0,0 +1,60 @@
+package main.sbxx.netty.demo2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import main.sbxx.netty.demo1.DiscardServerHander;
+
+/**
+ * @author Dair
+ * @since
+ */
+public class TimeServer {
+	
+	private int port;
+	
+	
+	public TimeServer(int port) {
+		this.port = port;
+	}
+	
+
+	public void run() throws InterruptedException {
+		
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			ServerBootstrap group = b.group(bossGroup, workerGroup);
+			group.channel(NioServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel socketChannel) throws Exception {
+							socketChannel.pipeline().addLast(new TimeServerHandler());
+						}
+					}).option(ChannelOption.SO_BACKLOG, 128);
+			
+			ChannelFuture f = b.bind(port).sync();
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		int port;
+		if (args.length > 0) {
+			port = Integer.parseInt(args[0]);
+		} else {
+			port = 8080;
+		}
+		new TimeServer(port).run();
+	}
+}
